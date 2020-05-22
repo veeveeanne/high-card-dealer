30f2ef0af18f2ab92ba70b1b33d9df753874ea72
Vivian Wang <vivian.emmenuel.wang@gmail.com>
2020-05-22 16:57:50 -0400

    Completed set up of deck

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
-const sayHello = () => {
-  alert("Hello World!")
+let Deck = require('./deck')
+
+let deck = new Deck
+
+let playGame = (deck) => {
+  Deck.shuffleCards(deck)
+  console.log(deck)
 }
 
-const suits = ['♦', '♣', '♠', '♥']
-const ranks = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K', 'A']
+playGame(deck)
