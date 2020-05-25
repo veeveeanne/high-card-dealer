eb3d2a7a3c2a719c99a3f34355c766d5e2ca9f37
Vivian Wang <vivian.emmenuel.wang@gmail.com>
2020-05-25 13:59:35 -0400

    Completed game logic using OOP

--- deck.js
+++ deck.js
@@ -2,30 +2,32 @@ let Card = require('./card')
 
 class Deck {
   constructor() {
+    this.cards = []
+  }
+
+  createDeck() {
     const suits = ['♦', '♣', '♠', '♥']
     const ranks = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K', 'A']
 
-    let cards = []
     suits.forEach((suit) => {
       ranks.forEach((rank) => {
-        cards.push(new Card(suit, rank))
+        this.cards.push(new Card(suit, rank))
       })
     })
-    return cards
   }
 
-  static shuffleCards(deck) {
-    let currentIndex = deck.length
+  shuffleCards() {
+    let currentIndex = this.cards.length
     let randomIndex
     let temporaryValue
 
     for (let i = currentIndex - 1; i > 0; i--) {
       randomIndex = Math.floor(Math.random() * i)
-      temporaryValue = deck[i]
-      deck[i] = deck[randomIndex]
-      deck[randomIndex] = temporaryValue
+      temporaryValue = this.cards[i]
+      this.cards[i] = this.cards[randomIndex]
+      this.cards[randomIndex] = temporaryValue
     }
-    return deck
+    return this.cards
   }
 
   static checkCards(deck) {
---
+++ hand.js
@@ -0,0 +1,27 @@
+class Hand {
+  constuctor() {
+    this.cards = []
+    this.value = 0
+  }
+
+  getValue() {
+    let value = 0
+    this.cards.forEach((card) => {
+      if (card.rank === 'J') {
+        value += 11
+      } else if (card.rank === 'Q') {
+        value += 12
+      } else if (card.rank === 'K') {
+        value += 13
+      } else if (card.rank === 'A') {
+        value += 14
+      } else {
+        value += card.rank
+      }
+    })
+    this.value = value
+    return this.value
+  }
+}
+
+module.exports = Hand
